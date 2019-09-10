311166a7d7f510aec2db6edda56572835c8337e2
Chance Hudson <jchancehud@gmail.com>
2019-09-09 19:53:31 -0500

    file select thingy

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
 export default class Home extends React.Component<{}> {
+  handleFileInput = (files: {
+    name: string,
+    lastModified: number,
+    lastModifiedDate: Date,
+  }[]) => {
+    if (files.length === 0) {
+      alert("No file selected")
+      return
+    } else if (files.length > 1) {
+      alert("Only one file may be selected")
+      return
+    }
+    const [file] = files
+    console.log(file)
+  }
+
   render() {
-    return (<></>)
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+        <div style={{
+          fontSize: 21,
+        }}>
+          WorkIt
+        </div>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e: any) => this.handleFileInput(e.target.files)}
+        />
+      </div>
+    )
   }
 }
